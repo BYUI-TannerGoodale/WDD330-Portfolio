5e67fbcedc49f0e3c538262b1d99c4855a3e5242
Tanner Goodale <goo18011@byui.edu>
2021-01-05 13:08:18 -0600

    init

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Primary function of this file is to populate the directory list from an array
+
+const links = [
+    {
+        label: "Week1 Notes",
+        url: "week1/index.html"
+    }
+];
+
+console.log(links.length);
+
+//Pull ol from document object and populate it with il elements using the links array as the data scourse
+
+let directory = document.getElementById(directory);
+
+directory.onload(function(){
+    links.forEach(link => {
+        var li = document.createElement("li");
+        li.value = link.label;
+        li.href = link.url;
+        directory.appendChild(li);
+    });
+})
