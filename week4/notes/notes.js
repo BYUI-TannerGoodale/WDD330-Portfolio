7e529593d273a5502b11e4ba4d2cf9673ccd94f7
Tanner Goodale <goo18011@byui.edu>
2021-01-26 22:29:08 -0600

    Week 4 notes

---
+++ week4/notes/notes.js
@@ -0,0 +1,8 @@
+const form = document.forms.search;
+
+////document.addEventListener(event, function, useCapture)
+//form.addEventListener('submit', search, false);
+//function search(event) {
+////Stop the defult redirect.
+//    event.preventDefault();
+//}
